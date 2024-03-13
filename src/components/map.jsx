6ef98377effb58e6b521c70b1c94e79fd2faaef9
mathieu.amacher <amacher_mathieu@hotmail.com>
2024-03-13 20:31:18 +0100

    map2

--- src/components/map.jsx
+++ src/components/map.jsx
@@ -1,20 +1,44 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Map from "react-map-gl";
+import { useAtom, useAtomValue } from "jotai";
+import { cityAtom } from '../atom/userAtom.jsx';
+import mapboxSdk from '@mapbox/mapbox-sdk/services/geocoding';
 
+import 'mapbox-gl/dist/mapbox-gl.css';
 
+const TOKEN = 'pk.eyJ1IjoibWF0aGlldWFtYWNoZXIiLCJhIjoiY2x0cG44eW5tMHMwaTJqbXA0aXczYXBsYiJ9.QgMMcu07S_F3aLEmwSC6WQ';
+const geocodingClient = mapboxSdk({ accessToken: TOKEN });
 
-import 'mapbox-gl/dist/mapbox-gl.css'
-
-
-const TOKEN='pk.eyJ1IjoibWF0aGlldWFtYWNoZXIiLCJhIjoiY2x0cG44eW5tMHMwaTJqbXA0aXczYXBsYiJ9.QgMMcu07S_F3aLEmwSC6WQ';
-
-export default function DisplayMap ()  {
+export default function DisplayMap () {
+  const city = useAtomValue(cityAtom);
   const [viewport, setViewport] = useState({
     longitude: 2.39,
     latitude: 48.88,
-    zoom: 10,
-    
+    zoom: 1,
   });
+
+  useEffect(() => {
+    if (city && typeof city === 'object' && city.city) {
+      geocodingClient.forwardGeocode({
+        query: city.city,
+        limit: 1
+      })
+      .then((response) => {
+        console.log('Geocoding Response:', response); // Afficher la réponse en console
+        if (response && response.body && response.body.features && response.body.features.length > 0) {
+          const feature = response.body.features[0];
+          setViewport({
+            longitude: feature.center[0],
+            latitude: feature.center[1],
+            zoom: 10,
+          });
+        }
+      })
+      .catch((error) => {
+        console.error("Error during geocoding:", error);
+      });
+    }
+  }, [city]);
 
   const handleScroll = (event) => {
     let newZoom = viewport.zoom;
@@ -30,6 +54,8 @@ export default function DisplayMap ()  {
       zoom: newZoom
     });
   };
+
+  console.log('le city Atom dans map', city)
 
   return (
     <div style={{ width: "100%", height: "40vh" }} onWheel={handleScroll}>
