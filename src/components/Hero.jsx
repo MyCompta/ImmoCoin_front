b30c93121f3a942cddfeb4f230fea3d424d2209d
LoulergueC <clement@loulergue.fr>
2024-03-14 17:51:37 +0000

    new favicon and logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import "./Hero.scss";
 import { Link } from "react-router-dom";
+import logo from "../assets/logo-no-background.svg";
 
 const Hero = () => {
   return (
     <div className="heroContainer">
       <div className="content">
+        <img src={logo} alt="logo" className="logo" />
         <h1>Find Your Ideal Home or Sell with Ease</h1>
         <h4>Welcome to ImmoCoin - the future of real estate starts here.</h4>
         <div className="buttons">
